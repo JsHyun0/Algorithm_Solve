38e7aff3b40343eb188ce9fd2db2f191d6ebc724
Jiseung Hyun <120002958+JsHyun0@users.noreply.github.com>
2025-01-24 14:48:09 +0900

    [Silver III] Title: 두 수의 합, Time: 52 ms, Memory: 7036 KB -BaekjoonHub

---
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+
+using namespace std;
+// 시간 초과
+int main() {
+    int N, x, result = 0;
+    cin >> N;
+    vector<int> ary(N, 0);
+    set<int> s;
+    for(int i = 0; i < N; i++) {
+        cin >> ary[i];
+        s.insert(ary[i]);
+    }
+    cin >> x;
+    sort(ary.begin(), ary.end());
+    for(int i = 0; i < N; i++) {
+        if(s.find(x-ary[i]) != s.end()) {
+            result++;
+        }
+
+    }
+    
+    
+    cout << result/2;
+    
+    return 0;
+}
